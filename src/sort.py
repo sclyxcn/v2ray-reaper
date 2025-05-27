fa1c18f06170fd0a30d122d33d761adeabbe9049
sclyxcn <sclyx@kingdee.ltd>
2025-05-27 13:35:37 +0800

    Update sort.py

--- src/sort.py
+++ src/sort.py
@@ -43,8 +43,12 @@ def main() -> None:
 
     for protocol, data in protocol_data.items():
         encoded_data = pybase64.b64encode(data.encode()).decode()
-        with (split_path / protocols[protocol]).open("w") as file:
-            file.write(encoded_data)
+        try:
+            with (split_path / protocols[protocol]).open("w") as file:
+                file.write(encoded_data)
+        except IsADirectoryError as e:
+            print(f"Error: {e}. The path '{split_path / protocols[protocol]}' is a directory, not a file.")       
+
 
 
 if __name__ == "__main__":
